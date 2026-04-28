923e9f5483d02a996c22b4fae00d8797259e34e8
PedroBibiano <pedrobibiano777@gmail.com>
2026-04-28 19:59:05 -0300

    feat(api): adiciona endpoints de listagem por ID e deleção no EventoController

--- EventClean/src/main/java/peabibiano/EventClean/infra/presentation/EventoController.java
+++ EventClean/src/main/java/peabibiano/EventClean/infra/presentation/EventoController.java
@@ -1,37 +1,52 @@
 package peabibiano.EventClean.infra.presentation;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import peabibiano.EventClean.core.entities.Evento;
 import peabibiano.EventClean.core.usecase.CriarEventoCase;
-import peabibiano.EventClean.core.usecase.ListarEventoCase;
 import peabibiano.EventClean.infra.dtos.EventoDto;
 import peabibiano.EventClean.infra.gateway.EventoRepositoryGateway;
 import peabibiano.EventClean.infra.mappers.EventoDtoMapper;
-import peabibiano.EventClean.infra.persistence.EventoEntity;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/")
+@CrossOrigin(origins = "*")
 public class EventoController {
 
     private final EventoRepositoryGateway eventoRepositoryGateway;
     private final EventoDtoMapper eventoDtoMapper;
-    private final CriarEventoCase criarEvento;
+    private final CriarEventoCase criarEventoCase;
 
-    public EventoController(EventoRepositoryGateway eventoRepositoryGateway, EventoDtoMapper eventoDtoMapper, CriarEventoCase criarEvento) {
+    public EventoController(EventoRepositoryGateway eventoRepositoryGateway, EventoDtoMapper eventoDtoMapper, CriarEventoCase criarEventoCase) {
         this.eventoRepositoryGateway = eventoRepositoryGateway;
         this.eventoDtoMapper = eventoDtoMapper;
-        this.criarEvento = criarEvento;
+        this.criarEventoCase = criarEventoCase;
     }
 
     @PostMapping("criarevento")
-    public EventoDto Criarevento(@RequestBody EventoDto eventoDto) {
+    public ResponseEntity<EventoDto> Criarevento(@RequestBody EventoDto eventoDto) {
         Evento novoEvento = criarEventoCase.execute(eventoDtoMapper.toDomain(eventoDto));
-        return eventoDtoMapper.toDto(novoEvento);
-    }
-    @GetMapping("listareventos")
-    public String Listareventos(){
-        return null;
+
+        ResponseEntity<EventoDto> response = ResponseEntity.ok(eventoDtoMapper.toDto(novoEvento));
+
+        return response;
     }
 
+    @GetMapping("listareventos")
+    public ResponseEntity<List<EventoDto>> Listareventos() {
+        List<Evento> eventosDomain = eventoRepositoryGateway.ListarEventos();
+
+        List<EventoDto> eventosDtos = eventosDomain.stream()
+                .map(eventoDtoMapper::toDto)
+                .toList();
+        return ResponseEntity.ok(eventosDtos);
+    }
+    @DeleteMapping("deletarevento/{id}")
+    public ResponseEntity<Void> Deletarevento(@PathVariable Long id) {
+        eventoRepositoryGateway.deletarEvento(id);
+        return ResponseEntity.ok().build();
+    }
 }
